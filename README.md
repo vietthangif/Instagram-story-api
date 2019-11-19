37a6ba8909fb9742ab3f8de6594718a04a24a5ab
Khanh Nguyen <khanh.nguyen.viet1989@gmail.com>
2019-11-19 13:39:37 +0700

    Add config.ts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # instagram-story-api
 
 Instagram story api
+
+Libraries:
+
+https://github.com/dilame/instagram-private-api
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.username = process.env.USERNAME || '0587900871';
+config.password = process.env.PASSWORD || 'jm@123456';
+
+module.exports = config;
